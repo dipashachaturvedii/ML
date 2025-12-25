f40da41c683df4bba4714f73fc7944349a40da86
dipasha <96734410+dipashachaturvedii@users.noreply.github.com>
2025-12-25 22:38:26 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+import email
+from os import replace
+from setuptools import find_packages, setup
+from typing import List
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+  '''
+  this returns the list of requirements
+  '''
+  requirements = []
+  with open(file_path) as file_obj:
+    requirements = file_obj.readlines()
+    requirements = [req.replace("\n"," ") for req in requirements]
+  if HYPHEN_E_DOT in requirements: 
+   requirements.remove(HYPHEN_E_DOT)
+  return requirements 
+
+setup(
+    name= 'Ml Project',
+    email= 'dipashachaturvedii@gmail.com',
+    version= '1.0.0',
+    author= 'Dipasha Chaturvedi',
+    packages=find_packages(),
+    install_requires= get_requirements('requirements.txt')  
+    )
